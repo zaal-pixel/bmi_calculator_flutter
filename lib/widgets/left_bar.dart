91e85813250cacf6b3354eeb7eae7b0a5d02a4cc
zaal-pixel <alzadinaya@gmail.com>
2021-11-28 14:19:06 +0100

    modify ver.1

--- lib/widgets/left_bar.dart
+++ lib/widgets/left_bar.dart
@@ -1,28 +1,24 @@
 import 'package:bmi/constants.dart';
 import 'package:flutter/material.dart';
 
-class LeftBar extends StatelessWidget {
-  final double barWidth;
-  const LeftBar ({ Key? key }) : super(key: key);
-  
+class RightBar extends StatelessWidget {
+  const RightBar({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Row(mainAxisAlignment: MainAxisAlignment.end,
-    children: [
-      Container( 
-        height:25,
-      width: barWidth,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(20),
-          bottomLeft: Radius.circular(20),
-
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Container(
+          height: 25,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(20),
+              bottomRight: Radius.circular(20),
+            ),
+          ),
         ),
-         ),
-        ),
-    ])
-      
+      ],
     );
   }
 }
